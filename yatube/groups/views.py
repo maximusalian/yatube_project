579d5866e5557690f5112e4d5bc58e9127735042
Maksim Agarkov <maxim32149@gmail.com>
2022-12-18 13:56:18 +0300

    add view-function and url

--- yatube/groups/views.py
+++ yatube/groups/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 # Create your views here.
-def group_posts(request):
-    return HttpResponse(f'Группа {any_slug}')
+def group_posts(request, slug):
+    return HttpResponse(f'Группа {slug}')
